8d8c32340daea198dbb0da90d6039cddc65bb21e
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-07-14 22:47:30 +0200

    Simplified internals of fields.

--- sphinxql/fields.py
+++ sphinxql/fields.py
@@ -9,17 +9,16 @@ class Field(columns.Column):
     attr_name is the attribute it uses
     from the ORM object.
     """
+    _type = None
 
-    def __init__(self, type, model_attr):
-        super(Field, self).__init__(type, None)
+    def __init__(self, model_attr):
+        super(Field, self).__init__(self._type, None)
         self.model_attr = model_attr
 
 
 class Text(Field):
     _sphinx_field_name = 'sql_field_string'
-
-    def __init__(self, model_attr):
-        super(Text, self).__init__(types.String, model_attr)
+    _type = types.String
 
 
 class Integer(Field):
@@ -27,9 +26,7 @@ class Integer(Field):
     A Sphinx attribute (i.e. not indexed)
     """
     _sphinx_field_name = 'sql_attr_bigint'
-
-    def __init__(self, model_attr):
-        super(Integer, self).__init__(types.Integer, model_attr)
+    _type = types.Integer
 
 
 class Float(Field):
@@ -37,9 +34,7 @@ class Float(Field):
     A Sphinx attribute (i.e. not indexed)
     """
     _sphinx_field_name = 'sql_attr_float'
-
-    def __init__(self, model_attr):
-        super(Float, self).__init__(types.Float, model_attr)
+    _type = types.Float
 
 
 class String(Field):
@@ -47,9 +42,7 @@ class String(Field):
     A Sphinx attribute (i.e. not indexed)
     """
     _sphinx_field_name = 'sql_attr_string'
-
-    def __init__(self, model_attr):
-        super(String, self).__init__(types.String, model_attr)
+    _type = types.String
 
 
 class DateTime(Field):
@@ -57,20 +50,14 @@ class DateTime(Field):
     A Sphinx attribute (i.e. not indexed)
     """
     _sphinx_field_name = 'sql_attr_timestamp'
-
-    def __init__(self, model_attr):
-        super(DateTime, self).__init__(types.DateTime, model_attr)
+    _type = types.DateTime
 
 
 class Date(Field):
     _sphinx_field_name = 'sql_attr_timestamp'
-
-    def __init__(self, model_attr):
-        super(Date, self).__init__(types.Date, model_attr)
+    _type = types.Date
 
 
 class Bool(Field):
     _sphinx_field_name = 'sql_attr_bool'
-
-    def __init__(self, model_attr):
-        super(Bool, self).__init__(types.Bool, model_attr)
+    _type = types.Bool
