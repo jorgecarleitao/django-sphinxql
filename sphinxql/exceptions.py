0fe88a1ebcbc3997e6b5a499c48802ca04adc86c
Jorge C. Leitão <jorgecarleitao@gmail.com>
2014-07-14 20:09:50 +0200

    Made all errors subclasses of a new Error - SphinxError.
    This allows to catch any Django-SphinxQL error.

--- sphinxql/exceptions.py
+++ sphinxql/exceptions.py
@@ -1,8 +1,16 @@
-class ImproperlyConfigured(Exception):
+
+class SphinxError(Exception):
+    """
+    general exception of Sphinx so users can catch any Sphinx error.
+    """
+    pass
+
+
+class ImproperlyConfigured(SphinxError):
     """somehow improperly configured"""
     pass
 
 
-class NotSupportedError(Exception):
+class NotSupportedError(SphinxError):
     """Something not support by Sphinx"""
     pass
